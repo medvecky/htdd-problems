bb449605b4d5d6f07d992544cf7047619c4c14c8
Alexey Medvecky <alexey.medvecky@gmail.com>
2019-10-06 22:43:23 +0300

    Add files via upload

--- complex-data/hp-family-listing.rkt
+++ complex-data/hp-family-listing.rkt
@@ -24,6 +24,22 @@
 ; will use this data definition throughout the rest of the homework.
 ; 
 
+; PROBLEM 2: 
+; 
+; Define a constant named ARTHUR that represents the descendant family tree for 
+; Arthur Weasley. You can find all the infomation you need by starting 
+; at: http://harrypotter.wikia.com/wiki/Arthur_Weasley.
+; 
+; You must include all of Arthur's children and these grandchildren: Lily, 
+; Victoire, Albus, James.
+; 
+; 
+; Note that on the Potter wiki you will find a lot of information. But for some 
+; people some of the information may be missing. Enter that information with a 
+; special value of "" (the empty string) meaning it is not present. Don't forget
+; this special value when writing your interp.
+; 
+
 
 (define-struct character (name patronus wand children))
 ;; Character is (make-character String String String ListOfCharacter
@@ -179,25 +195,6 @@
               (fn-for-loc (rest loc)))])) 
 
 
-
-
-; PROBLEM 2: 
-; 
-; Define a constant named ARTHUR that represents the descendant family tree for 
-; Arthur Weasley. You can find all the infomation you need by starting 
-; at: http://harrypotter.wikia.com/wiki/Arthur_Weasley.
-; 
-; You must include all of Arthur's children and these grandchildren: Lily, 
-; Victoire, Albus, James.
-; 
-; 
-; Note that on the Potter wiki you will find a lot of information. But for some 
-; people some of the information may be missing. Enter that information with a 
-; special value of "" (the empty string) meaning it is not present. Don't forget
-; this special value when writing your interp.
-; 
-
-
 ; PROBLEM 3:
 ; 
 ; Design a function that produces a pair list (i.e. list of two-element lists)
@@ -209,6 +206,62 @@
 ; 
 
 
+;; Character -> ListOfPairsOfString
+;; ListOfCharacter -> ListOfPairsOfString
+;; produce list of pairs (Name Patronus) for all element of tree
+(check-expect (name-patronus-list-for--character FREDW) (list (list "Fred Weasley" "Magpie")))
+(check-expect (name-patronus-list-for--loc (list FREDW CHARLIEW)) (list (list "Fred Weasley" "Magpie") (list "Charlie Weasley" "Non-corporeal")))
+(check-expect (name-patronus-list-for--character ARTHURW)
+             (list
+              (list "Arthur Weasley" "Weasel")
+              (list "Bill Weasley" "Non-corporeal")
+              (list "Victoire Weasley" "")
+              (list "Dominique Weasley" "")
+              (list "Louis Weaslye" "")
+              (list "Charlie Weasley" "Non-corporeal")
+              (list "Percy Weasley" "Non-corporeal")
+              (list "Molly Weasley 2" "")
+              (list  "Lucy Weasley" "")
+              (list "Fred Weasley" "Magpie")
+              (list "George Weasley" "Magpie")
+              (list "Fred Weasley 2" "")
+              (list "Roxanne Weasley" "")
+              (list "Ron Weasley" "Jack Russel Terrier")
+              (list "Rose Granger-Weasley" "")
+              (list "Hugo Granger-Weasley" "")
+              (list "Ginevra Weasley" "Horse")
+              (list "James Sirius Potter 2" "")
+              (list "Albus Potter" "")
+              (list "Lilly Potter 2" "")))            
+              
+; (define (name-patronus-list-for--character c) (list (list "" ""))) ; stub
+; (define (name-patronus-list-for--loc loc) (list (list "" ""))) ; stub
+#;
+(define (fn-for-character c)
+  (... (character-name c)                    ; String
+       (character-patronus c)                ; String
+       (character-wand c)                    ; String
+       (fn-for-loc (character-children c)))) 
+
+#;
+(define (fn-for-loc loc)
+  (cond [(empty? loc) (...)]
+        [else
+         (... (fn-for-character (first loc))
+              (fn-for-loc (rest loc)))]))
+
+(define (name-patronus-list-for--character c)
+  (cons
+   (list (character-name c)                   
+         (character-patronus c))                                   
+         (name-patronus-list-for--loc (character-children c)))) 
+
+(define (name-patronus-list-for--loc loc)
+  (cond [(empty? loc) empty]
+        [else
+         (append (name-patronus-list-for--character (first loc))
+                 (name-patronus-list-for--loc (rest loc)))]))
+
 ; PROBLEM 4:
 ; 
 ; Design a function that produces the names of every person in a given tree 
@@ -217,3 +270,60 @@
 ; You must use ARTHUR as one of your examples.
 ; 
 
+
+;; Character -> ListOfStrings
+;; ListOfCharacter -> ListOfStrings
+;; produces list of character's name who had wands from consumed matherial
+(check-expect (use-wand--caracater ARTHURW "Gold") empty)
+(check-expect (use-wand--caracater ARTHURW "Yew") (list "Ginevra Weasley"))
+(check-expect (use-wand--caracater ARTHURW "") (list
+               "Arthur Weasley"
+               "Bill Weasley" 
+               "Victoire Weasley"
+               "Dominique Weasley"
+               "Louis Weaslye"
+               "Percy Weasley" 
+               "Molly Weasley 2"
+               "Lucy Weasley" 
+               "Fred Weasley"
+               "George Weasley" 
+               "Fred Weasley 2" 
+               "Roxanne Weasley" 
+               "Rose Granger-Weasley"
+               "Hugo Granger-Weasley" 
+               "James Sirius Potter 2"
+               "Albus Potter" 
+               "Lilly Potter 2"))            
+              
+;(define (use-wand--caracater c w) empty) ; stub
+;(define (use-wand--caracater loc w) empty) ; stub
+#;
+(define (fn-for-character c)
+  (... (character-name c)                    ; String
+       (character-patronus c)                ; String
+       (character-wand c)                    ; String
+       (fn-for-loc (character-children c)))) 
+
+#;
+(define (fn-for-loc loc)
+  (cond [(empty? loc) (...)]
+        [else
+         (... (fn-for-character (first loc))
+              (fn-for-loc (rest loc)))]))
+
+
+(define (use-wand--caracater c w)
+   (if (string=? (character-wand c) w)
+       (cons
+        (character-name c)
+        (use-wand--loc (character-children c) w))
+       (use-wand--loc (character-children c) w)))
+                 
+
+(define (use-wand--loc loc w)
+  (cond [(empty? loc) empty]
+        [else
+         (if (not (empty? (use-wand--caracater (first loc) w)))
+             (append (use-wand--caracater (first loc) w) 
+              (use-wand--loc (rest loc) w))
+             (use-wand--loc (rest loc) w))]))
