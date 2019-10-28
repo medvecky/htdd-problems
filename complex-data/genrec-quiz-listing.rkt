9e54607dd4389338b2d913e8841673ea3c291413
Alexey Medvecky <alexey.medvecky@gmail.com>
2019-10-28 22:30:26 +0200

    Add files via upload

--- complex-data/genrec-quiz-listing.rkt
+++ complex-data/genrec-quiz-listing.rkt
@@ -2,6 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname genrec-quiz-listing) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
+(require racket/list)
 
 ;  PROBLEM 1:
 ;  
@@ -21,9 +22,9 @@
 ;; produce a circle fractal of size n and colour c
 (check-expect (cicrcle-fractal 5 "red") (circle 5 "outline" "red"))
 (check-expect (cicrcle-fractal 10 "red") (overlay
-                                        (circle 10 "outline" "red")
-                                        (beside (circle 5 "outline" "red")
-                                                (circle 5 "outline" "red"))))
+                                          (circle 10 "outline" "red")
+                                          (beside (circle 5 "outline" "red")
+                                                  (circle 5 "outline" "red"))))
 ;(define (circle-fractal n c) empty-image) ; stub
 
 #;
@@ -94,6 +95,18 @@
 (define B4 (list "X" "O" "X"
                  "O" "O" "X"
                  "X" "X" "O"))
+
+(define B5 (list "X" "O" "X"
+                 "O" false "X"
+                 "X" "X" "O"))
+
+(define B6 (list "X" "O" "X"
+                 "O" false "X"
+                 "X" "X" false))
+
+(define B7 (list "X" "O" "X"
+                 "O" "O" "X"
+                 "O" "X" "O"))
  
 (define (fn-for-board b)
   (cond [(empty? b) (...)]
@@ -116,10 +129,18 @@
     (count--board bd)))
 
 
+;; Board -> Number
+;; Produce number of achievable boards from consumed board
+
+;(check-expect (count-boards B4) 0)
+(check-expect (count-boards B5) 2)
+(check-expect (count-boards B6) 4)
+(check-expect (count-boards B1) 8)
+
 (define (count-boards bd)
   (local [(define (count--board bd)
             (if (trivial-solution? bd)
-                  1
+                1
                 (count--lob (next-boards bd))))
           (define (count--lob lob) 
             (cond [(empty? lob) 0]
@@ -142,14 +163,68 @@
 ;; Board -> (listof Board)
 ;; produce a list of all boards
 ;; which can be achieved from consumed board
-;; !!!
 
-(define (next-boards b) empty) ; stub
+(check-expect (next-boards B0)
+              (list
+               (list "X" false false
+                     false false false
+                     false false false)
+               (list "O" false false
+                     false false false
+                     false false false)))
 
+(check-expect (next-boards (list "X" false false
+                                 false false false
+                                 false false false))
+              (list
+               (list "X"  "X" false
+                     false false false
+                     false false false)
+               (list "X" "O" false
+                     false false false
+                     false false false)))
 
+; (define (next-boards b) empty) ; stub
 
+(define (next-boards b)
+  (fill-x-o (find-blank b) b))
 
+;; Board -> Number
+;; Produce first blank field's number
 
+(check-expect (find-blank B0) 0)
+(check-expect (find-blank B2) 7)
+
+; (define (find-blank b) -1) ; stub
+
+(define (find-blank b)
+  (cond [(empty? b) (error "The board didn't have a blank space.")]
+        [else
+         (if (false? (first b))
+             0
+             (+ 1 (find-blank (rest b))))]))
+
+;; Position Board -> (listof Board)
+;; Produce a list of boards
+;; were the field with consumed position filled with X and O
+(check-expect (fill-x-o 0 B0)
+              (list
+               (cons "X" (rest B0))
+               (cons "O" (rest B0))))
+
+(check-expect (fill-x-o 1 B0)
+              (list
+               (cons (first B0) (cons "X" (rest (rest B0))))
+               (cons (first B0) (cons "O" (rest (rest B0))))))
+
+; (define (fill-x-o p b) empty) ; stub
+
+(define (fill-x-o p b)
+  (local [(define (filler n)
+            (cond
+              [(= n 0) (list-set b p "X")]
+              [(= n 1) (list-set b p "O")]))]
+    (build-list 2 filler)))
 
 ;  PROBLEM 3:
 ;  
@@ -164,3 +239,54 @@
 ;  
 
 
+
+;; Board -> Number
+;; Produce number of achievable boards from consumed board
+
+;(check-expect (count-boards B4) 0)
+(check-expect (count-valid-boards B5) 1)
+(check-expect (count-valid-boards B6) 1)
+(check-expect (count-valid-boards B1) 3)
+
+(define (count-valid-boards bd)
+  (local [(define (count--board bd)
+            (if (trivial-solution? bd)
+                (if (valid-board?  bd)
+                    1
+                    0)
+                (count--lob (next-boards bd))))
+          (define (count--lob lob) 
+            (cond [(empty? lob) 0]
+                  [else
+                   (+ (count--board (first lob))
+                      (count--lob (rest lob)))]))]
+    (count--board bd)))
+
+
+;; Board -> Boolean
+;; Produce true when Board is valid
+;; contains 5 X and 4 0
+
+(check-expect (valid-board? B4) true)
+(check-expect (valid-board? B7) false)
+
+; (define (valid-board? b) false) ; stub
+
+(define (valid-board? b)
+  (and (= 4 (count-on-board "O" b)) (= 5 (count-on-board "X" b))))
+
+;; Symbol Board -> Number
+;; Produce a number of the consumed symbol on consumed board
+
+(check-expect (count-on-board "X" B4) 5)
+(check-expect (count-on-board "O" B4) 4)
+; (define (count-on-board s b) 0) ; stub
+
+(define (count-on-board s b)
+  (cond
+    [(empty? b) 0]
+    [else (+
+           (if (eq? (first b) s)
+               1
+               0)
+           (count-on-board s (rest b)))]))
